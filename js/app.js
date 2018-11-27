312449d95763b7aa1d18e505b719429de3b6c1be
Jasmin <jasmin.arensdorf@gmail.com>
2018-11-27 14:28:58 -0800

    Modified if statement to include all caps response.  Also added second guess question.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var playGame = prompt('Would you like to play my guessing game (please enter y or n)?');
+var playGame = prompt('Would you like to play my guessing game (please enter yes or no)?');
 
 var answer;
 //var correctGuess = false;
@@ -11,21 +11,39 @@ playGame.toUpperCase();
 
 if(playGame === 'y' || playGame === 'yes' || playGame === 'YES') {
   alert('Great! Here we go');
-  console.log('Hooray! The user is going to play my guessing game.');
+  console.log('Hooray! The user answered "' + playGame + '" and will play my guessing game.');
   
-  answer = prompt('Can I speak French (please enter y or n)?');
+  answer = prompt('Can I speak French (please enter yes or no)?');
   answer.toLowerCase();
 
-  if(answer === 'n' || answer ==='no' || answer === 'NO') {
+  if(answer === 'y' || answer ==='yes' || answer === 'YES') {
     //correctGuess = true;
-    alert('The user guessed "' + answer + '" that I can speak French.  You are correct!');
-    console.log('The user guessed "' + answer + '" I can speak French.  You are correct!');
-  } else {
     alert('The user guessed "' + answer + '" that I can speak French.  Oops wrong answer.');
-    console.log('the user guessed "' + answer + '" that I can speak French.  Oops wrong answer');
+    console.log('The user guessed "' + answer + '" that I can speak French.  Oops wrong answer.');
+  } else { // User answered either no, n or N
+    alert('The user guessed "' + answer + '" that I cannot speak French.  You are correct!');
+    console.log('The user guessed "' + answer + '" I cannot speak French.  You are correct!');
   }
+  
+  // if(answer === 'n' || answer ==='no' || answer === 'NO') {
+  //   //correctGuess = true;
+  //   alert('The user guessed "' + answer + '" that I can speak French.  You are correct!');
+  //   console.log('The user guessed "' + answer + '" I can speak French.  You are correct!');
+  // } else {
+  //   alert('The user guessed "' + answer + '" that I can speak French.  Oops wrong answer.');
+  //   console.log('the user guessed "' + answer + '" that I can speak French.  Oops wrong answer');
+  // }
 
+  answer = prompt('Do I like to watch science fiction movies (please answer yes or no)?');
+  answer.toLowerCase();
 
+  if(answer === 'y' || answer === 'yes' || answer === 'YES') {
+    alert('The user guessed "' + answer + '" that I watch science fiction.  Correct!  I love science fiction flicks.');
+    console.log('The user guessed "' + answer + '" that I watch science fiction movies.  Correct!');
+  } else {
+    alert('The user guessed "' + answer + '" that I don\'t watch science fiction.  Oops wrong answer. I\'m a science fiction junky.');
+    console.log('The user guessed "' + answer + '" that I don\'t watch science fiction movies.  Oops wrong answer.  I\'m a science fiction junky.');
+  }
 
 
 } else {
