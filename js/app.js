87b0ef85dbea5e77d552c71e7117a46ab2a0e5b9
Jasmin <jasmin.arensdorf@gmail.com>
2018-11-27 13:58:58 -0800

    Coded the first question to guessing game.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,35 @@
 'use strict';
 
+var playGame = prompt('Would you like to play my guessing game (please enter y or n)?');
+
+var answer;
+//var correctGuess = false;
+
+alert('User entered: ' + playGame);
+
+playGame.toUpperCase();
+
+if(playGame === 'y' || playGame === 'yes' || playGame === 'YES') {
+  alert('Great! Here we go');
+  console.log('Hooray! The user is going to play my guessing game.');
+  
+  answer = prompt('Can I speak French (please enter y or n)?');
+  answer.toLowerCase();
+
+  if(answer === 'n' || answer ==='no' || answer === 'NO') {
+    //correctGuess = true;
+    alert('The user guessed "' + answer + '" that I can speak French.  You are correct!');
+    console.log('The user guessed "' + answer + '" I can speak French.  You are correct!');
+  } else {
+    alert('The user guessed "' + answer + '" that I can speak French.  Oops wrong answer.');
+    console.log('the user guessed "' + answer + '" that I can speak French.  Oops wrong answer');
+  }
+
+
+
+
+} else {
+  alert('Okay.  Try again next time.');
+  console.log('Oh well.  The user does not want to play my guessing game.');
+}
+
