eff2559ea7cea694b821a2117e3fdd661a7266c3
Jasmin <jasmin.arensdorf@gmail.com>
2018-11-27 15:02:59 -0800

    Added some css style.  Included to the reset.css and my styles.css file in index.html file.  Added the remaining questions to guessing game.

--- js/app.js
+++ js/app.js
@@ -3,39 +3,30 @@
 var playGame = prompt('Would you like to play my guessing game (please enter yes or no)?');
 
 var answer;
-//var correctGuess = false;
 
 alert('User entered: ' + playGame);
 
-playGame.toUpperCase();
+// Convert response to lower case
+playGame = playGame.toLowerCase();
 
-if(playGame === 'y' || playGame === 'yes' || playGame === 'YES') {
+if(playGame === 'y' || playGame === 'yes') {
   alert('Great! Here we go');
   console.log('Hooray! The user answered "' + playGame + '" and will play my guessing game.');
   
   answer = prompt('Can I speak French (please enter yes or no)?');
-  answer.toLowerCase();
+  answer = answer.toLowerCase();
 
-  if(answer === 'y' || answer ==='yes' || answer === 'YES') {
-    //correctGuess = true;
+  if(answer === 'y' || answer ==='yes') {
+
     alert('The user guessed "' + answer + '" that I can speak French.  Oops wrong answer.');
     console.log('The user guessed "' + answer + '" that I can speak French.  Oops wrong answer.');
   } else { // User answered either no, n or N
     alert('The user guessed "' + answer + '" that I cannot speak French.  You are correct!');
     console.log('The user guessed "' + answer + '" I cannot speak French.  You are correct!');
   }
-  
-  // if(answer === 'n' || answer ==='no' || answer === 'NO') {
-  //   //correctGuess = true;
-  //   alert('The user guessed "' + answer + '" that I can speak French.  You are correct!');
-  //   console.log('The user guessed "' + answer + '" I can speak French.  You are correct!');
-  // } else {
-  //   alert('The user guessed "' + answer + '" that I can speak French.  Oops wrong answer.');
-  //   console.log('the user guessed "' + answer + '" that I can speak French.  Oops wrong answer');
-  // }
 
   answer = prompt('Do I like to watch science fiction movies (please answer yes or no)?');
-  answer.toLowerCase();
+  answer = answer.toLowerCase();
 
   if(answer === 'y' || answer === 'yes' || answer === 'YES') {
     alert('The user guessed "' + answer + '" that I watch science fiction.  Correct!  I love science fiction flicks.');
@@ -45,6 +36,29 @@ if(playGame === 'y' || playGame === 'yes' || playGame === 'YES') {
     console.log('The user guessed "' + answer + '" that I don\'t watch science fiction movies.  Oops wrong answer.  I\'m a science fiction junky.');
   }
 
+  answer = prompt('Have I ever snorkled (please answer yes or no)?');
+  answer = answer.toLowerCase();
+
+  if(answer === 'y' || answer === 'yes') {
+    alert('The user guessed "' + answer + '" that I have snorkled.  Correct!');
+    console.log('The user guessed "' + answer + '" that I have snorked.  Correct!');
+  } else {
+    alert('The user guessed "' + answer + '" that I have not snorkled.  Oops wrong answer.');
+    console.log('The user guessed "' + answer + '" that I have not snorkled.  Oops wrong answer.');
+  }
+
+  answer = prompt('Have I gone zip lining (please answer yes or no)?');
+  answer = answer.toLowerCase();
+
+  if(answer === 'n' || answer === 'no') {
+    alert('The user guessed "' + answer + '" I have not zip lined.  Wrong answer.  I love to zip line.');
+    console.log('The user guessed "' + answer + '" I have not zip lined.  Wrong answer.  I love to zip line.');
+  } else {
+    alert('The user guessed correctly.  I have zip lined.  It was a blast!');
+    console.log('The user guessed correctly.  I have zip lined.  It was a blast!');
+  }
+  
+  alert('Thanks for playing my guessing game.');
 
 } else {
   alert('Okay.  Try again next time.');
